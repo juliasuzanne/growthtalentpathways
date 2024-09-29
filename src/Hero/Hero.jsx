96013846996c5ffd637782e922a6ce1e584b5c38
juliasuzanne <julia.s.grimes@gmail.com>
2024-09-28 22:41:02 -0400

    Add hero art

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -9,15 +9,24 @@ export default function Hero() {
         Getting your first ‘real job’ or making a career change in your early years{" "}
         <span className="bold-italic"> can be overwhelming.</span>
       </h1>
-      <p className="hero-p">
-        Our Early Career Coaching (ECC) program is crafted to help you overcome challenges and position yourself for
-        success in today’s competitive job market. Whether you’re a recent graduate or looking to pivot, we provide the
-        tools and support you need to land the right job and build a fulfilling career.
-      </p>
-      <div className="hero-bg" />
-      <div className="hero-button-group">
-        <button className="white">Contact</button>
-        <button className="whiteoutline">Services</button>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 col-sm-12">
+            <p className="hero-p">
+              Our Early Career Coaching (ECC) program is crafted to help you overcome challenges and position yourself
+              for success in today’s competitive job market. Whether you’re a recent graduate or looking to pivot, we
+              provide the tools and support you need to land the right job and build a fulfilling career.
+            </p>
+            <div className="hero-button-group">
+              <button className="white">Contact</button>
+              <button className="whiteoutline">Services</button>
+            </div>
+          </div>
+          <div className="col-lg-6 col-sm-12">
+            <img className="hero-figures" src="/figure-art3.png" />
+          </div>
+        </div>
       </div>
     </div>
   );
