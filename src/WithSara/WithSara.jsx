7969b0fe6e399b91a9a6d6e7186e105324274741
juliasuzanne <julia.s.grimes@gmail.com>
2024-09-30 17:40:45 -0400

    Responsive ws and contact

--- src/WithSara/WithSara.jsx
+++ src/WithSara/WithSara.jsx
@@ -3,27 +3,37 @@ import "./withsara.css";
 export default function WithSara() {
   return (
     <div className="ws-main">
-      <img className="ws_portrait" src="/GTP_SaraPortrait.png" />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-4 col-sm-12">
+            <img className="ws_portraitA" src="/GTP_SaraPortrait.png" />
+          </div>
+          <div className="col-lg-8 col-sm-12">
+            <div className="ws-textmain">
+              <h1>
+                <span className="ws-title1">Working</span> <span className="ws-title20"> with </span> <br></br>
+                <span className="ws-title21"> with </span> <span className="ws-title3">Sara</span>
+              </h1>
+              <p className="ws-p">
+                After spending many years in recruitment, I have unique insight into the interview and talent
+                acquisition processes. My passion is helping my clients develop essential habits to navigate the
+                transition from college life to the workplace, reducing stress through practical strategies.
+              </p>
 
-      <div className="ws-textmain">
-        <h1>
-          <span className="ws-title1">Working</span> <br></br>
-          <span className="ws-title2"> with </span> <span className="ws-title3">Sara</span>
-        </h1>
-        <p className="ws-p">
-          After spending many years in recruitment, I have unique insight into the interview and talent acquisition
-          processes. My passion is helping my clients develop essential habits to navigate the transition from college
-          life to the workplace, reducing stress through practical strategies.
-        </p>
-        <div className="ws-buttons">
-          <a href="https://www.linkedin.com/in/sarakontalonis/" target="_blank">
-            <button className="black">LinkedIn</button>
-          </a>
-          <a href="/SJKResumeGTS.pdf" download>
-            <button className="blackoutline">CV</button>{" "}
-          </a>
+              <div className="ws-buttons">
+                <a href="https://www.linkedin.com/in/sarakontalonis/" target="_blank">
+                  <button className="black">LinkedIn</button>
+                </a>
+                <a href="/SJKResumeGTS.pdf" download>
+                  <button className="blackoutline">CV</button>{" "}
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-sm-12">
+            <img className="ws_portraitB" src="/GTP_SaraPortrait.png" />
+          </div>
         </div>
-
         <br></br>
         {/* <Spacer /> */}
       </div>
