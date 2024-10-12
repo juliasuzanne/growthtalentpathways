d54c528c94224d39e638e980ee2396a419d51020
juliasuzanne <julia.s.grimes@gmail.com>
2024-10-11 20:38:04 -0400

    Revert "add Will's edits and fix send button on mobile font color"
    This reverts commit 974eaee5950fbd40a39004a68236e7c7f683765c.

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
       <img className="hero-logo" src="/logo-website.png" />
 
       <h1 className="hero-text">
-        Landing your first ‘real job’ or making a career change in your early years{" "}
+        Getting your first ‘real job’ or making a career change in your early years{" "}
         <span className="bold-italic"> can be overwhelming.</span>
       </h1>
 
@@ -27,10 +27,9 @@ export default function Hero() {
           </div> */}
           <div className="hero-row col-lg-6 col-sm-12">
             <p className="hero-p">
-              Our <span className="hero-p-bold"> Early Career Coaching (ECC) </span> program is crafted to help you
-              overcome challenges and position yourself for success in today’s competitive job market. Whether you’re a
-              recent graduate or looking to pivot, we provide the tools and support you need to land the right job and
-              build a fulfilling career.
+              Our Early Career Coaching (ECC) program is crafted to help you overcome challenges and position yourself
+              for success in today’s competitive job market. Whether you’re a recent graduate or looking to pivot, we
+              provide the tools and support you need to land the right job and build a fulfilling career.
             </p>
             <div className="hero-button-groupLG">
               <a href="#contact">
